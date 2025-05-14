c143bdeeeda62e11b6b8cacb733e81236f17dc44
Paolo Iovino <paolo@felicitygames.com>
2025-05-14 18:13:34 +0200

    -

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "felicitygames-Cup Quest-3.5.5";
+const contentToCache = [
+    "Build/F_Netlify.loader.js",
+    "Build/F_Netlify.framework.js",
+    "Build/F_Netlify.data",
+    "Build/F_Netlify.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
